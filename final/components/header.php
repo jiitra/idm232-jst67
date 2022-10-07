c12f3e8eafdd71356c8ea6eabeca0f1719500643
jiitra <74521649+jiitra@users.noreply.github.com>
2022-10-07 10:59:56 -0400

    removing css reset

--- final/components/header.php
+++ final/components/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF=8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>IDM232 Assignment 01</title>
-    <link rel="stylesheet" href="reset.css"/>
+    <!-- <link rel="stylesheet" href="reset.css"/> -->
     <link rel="stylesheet" href="styles.css"/>
 </head>
 
