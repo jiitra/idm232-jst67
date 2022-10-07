937972b60853260a4eb3f4b5043eedc120ba7dce
jiitra <74521649+jiitra@users.noreply.github.com>
2022-10-07 10:57:27 -0400

    fixing nested git repo

---
+++ final/components/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF=8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>IDM232 Assignment 01</title>
+    <link rel="stylesheet" href="reset.css"/>
+    <link rel="stylesheet" href="styles.css"/>
+</head>
+
+<body>
+    <script src="main.js"></script>
+</body>
+</html>
---
+++ final/index.php
@@ -0,0 +1,9 @@
+<?php include_once 'components/header.php'; ?>
+
+<?php
+$myVariable = 'Assignment - 01';
+?>
+
+<h1><?php echo $myVariable; ?></h1>
+
+<?php include_once 'components/footer.php'; ?>
