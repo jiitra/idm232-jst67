975d1b0d09e48c8fa84dc23076e37ebc42da1a26
jiitra <74521649+jiitra@users.noreply.github.com>
2022-12-07 15:33:56 -0500

    recipe validator

--- final/components/recipe_view.php
+++ final/components/recipe_view.php
@@ -1,17 +1,17 @@
 
 <div class="centered" id="recipe-img">
-    <?php echo "<img src=$img />"; ?>
+    <?php echo "<img src=$img alt=''>"; ?>
 </div>
 
 <div class="page">
     <h2 class="grid_header">Ingredients</h2>
-    <ul class="recipe_list recipe_list_ul">
-    <?php echo $recipe['ingredients']?>
-    </ul>
+    <div class="recipe_list recipe_list_ul">
+        <?php echo $recipe['ingredients']?>
+    </div>
 <div class="spacer"> </div>
     <h2 class="grid_header">How To Make</h2>
 
-    <ol class="recipe_list recipe_list_ol">
+    <div class="recipe_list recipe_list_ol">
         <?php echo $recipe['howto']?>
-    </ol>
+    </div>
 </div>
