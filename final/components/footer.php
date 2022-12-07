896cac38e27359c978f831d2646c059ec487c68c
jiitra <74521649+jiitra@users.noreply.github.com>
2022-12-07 15:22:10 -0500

    validator

--- final/components/footer.php
+++ final/components/footer.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF=8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>IDM232 Assignment 01</title>
-    <link rel="stylesheet" href="reset.css"/>
-    <link rel="stylesheet" href="styles.css"/>
-</head>
-
-<body>
-    <script src="main.js"></script>
+<script src="main.js"></script>
 
     <script>
     tinymce.init({
